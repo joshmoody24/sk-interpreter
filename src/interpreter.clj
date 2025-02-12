5776b97ce251bf9c1f4257ee83020955dc292936
joshmoody24 <joshmoody24@gmail.com>
2025-02-12 02:58:00 -0700

    Interpreter boilerplate

---
+++ src/interpreter.clj
@@ -0,0 +1,37 @@
+(ns interpreter
+  (:gen-class)
+  (:require
+   [parser] [clojure.string :as string]))
+
+;; --- Pretty Printing ---
+
+(defn pretty-print [expr]
+  (cond
+    (= (:type expr) :combinator) (:value expr)
+    (= (:type expr) :placeholder) (:name expr)
+    (= (:type expr) :application)
+    (str "(" (string/join " " (map pretty-print (:expressions expr))) ")")
+    (= (:type expr) :let)
+    (str "(let " (:name (:placeholder expr)) " = " (pretty-print (:value expr))
+         " in " (pretty-print (:body expr)) ")")
+    :else (str expr)))
+
+(defn evaluate [expr]
+  (throw (Exception. "Not implemented")))
+
+;; --- Main Execution ---
+
+(defn run []
+  (if (empty? *command-line-args*)
+    (println "Usage: clojure -m interpreter <filename>")
+    (let [filename (first *command-line-args*)
+          input (slurp filename)
+          parsed (parser/parse input)]
+      (if (:success parsed)
+        (do
+          (println "Parsed expression: " (pretty-print (:result parsed)))
+          (println "Evaluated result: " (pretty-print (evaluate (:result parsed)))))
+        (println "Parsing failed:" (:error parsed))))))
+
+(run)
+

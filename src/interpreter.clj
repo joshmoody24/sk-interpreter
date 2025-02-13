5aa5ff8a8bbd7f8d4b30ac3e0fd20f6b6add800a
joshmoody24 <joshmoody24@gmail.com>
2025-02-13 00:19:06 -0700

    Somewhat improved interpreter

--- src/interpreter.clj
+++ src/interpreter.clj
@@ -4,26 +4,37 @@
    [parser]
    [clojure.string :as string]))
 
-;; --- Pretty Printing ---
+;; --- Helper Function for applying functions, unwrapping metadata maps ---
+(defn call-fn [f arg]
+  (if (and (map? f) (contains? f :function))
+    ((:function f) arg)
+    (f arg)))
 
+;; --- Pretty Printing ---
 (defn pretty-print [ast]
   (cond
-    (vector? ast) (string/join "\n" (map pretty-print ast))
+    (vector? ast)
+    (string/join "\n" (map pretty-print ast))
 
     (map? ast)
-    (case (:type ast)
-      :combinator (:name ast)
-      :derived-combinator (:name ast)
-      :application-expression
-      (str "(" (string/join " " (map pretty-print (:expressions ast))) ")")
-      :definition
-      (str (:name ast) " = " (pretty-print (:expression ast)) ";")
-      (if (and (contains? ast :definitions)
-               (contains? ast :expression))
-        (str (pretty-print (:definitions ast))
-             "\n"
-             (pretty-print (:expression ast)))
-        (pr-str ast)))
+    (cond
+      (and (contains? ast :name) (contains? ast :definition) (contains? ast :function))
+      (str (:name ast) " = " (:definition ast))
+
+      :else
+      (case (:type ast)
+        :combinator (:name ast)
+        :derived-combinator (:name ast)
+        :application-expression
+        (str "(" (string/join " " (map pretty-print (:expressions ast))) ")")
+        :definition
+        (str (:name ast) " = " (pretty-print (:expression ast)) ";")
+        (if (and (contains? ast :definitions)
+                 (contains? ast :expression))
+          (str (pretty-print (:definitions ast))
+               "\n"
+               (pretty-print (:expression ast)))
+          (pr-str ast))))
 
     :else (str ast)))
 
@@ -38,13 +49,20 @@
 (defn interpret-combinator [combinator]
   (println "Interpreting combinator" combinator)
   (case (:name combinator)
-    "S" (fn [x] (fn [y] (fn [z] ((x z) (y z)))))
-    "K" (fn [x] (fn [_] x))
+    "S" {:name "S"
+         :definition "S"
+         :function (fn [x]
+                     (fn [y]
+                       (fn [z]
+                         (call-fn (call-fn x z) (call-fn y z)))))}
+    "K" {:name "K"
+         :definition "K"
+         :function (fn [x]
+                     (fn [_] x))}
     (throw (Exception. (str "Unknown base combinator: " (:name combinator))))))
 
 (declare interpret-primary-expression)
 
-;; Must be lazy
 (defn interpret-application-expression [expr env]
   (println "Interpreting application expression" expr)
   (if (= (:type expr) :application-expression)
@@ -52,23 +70,33 @@
           head (interpret-primary-expression (first expressions) env)
           tail (map #(interpret-primary-expression % env) (rest expressions))]
       (reduce (fn [f arg]
-                (if (fn? f)
-                  (f arg)
-                  (throw (Exception. (str "Expected function, got: " (pr-str f))))))
+                (call-fn f arg))
               head tail))
     (interpret-primary-expression expr env)))
 
 (defn interpret-primary-expression [expr env]
   (println "Interpreting primary expression" expr)
   (cond
-    (= (:type expr) :combinator) (interpret-combinator expr)
-    (= (:type expr) :derived-combinator) (interpret-derived-combinator expr env)
-    (= (:type expr) :application-expression) (interpret-application-expression expr env)
-    :else (throw (Exception. (pr-str "Expected primary expression, got: " expr)))))
+    (= (:type expr) :combinator)
+    (interpret-combinator expr)
+
+    (= (:type expr) :derived-combinator)
+    (interpret-derived-combinator expr env)
+
+    (= (:type expr) :application-expression)
+    (interpret-application-expression expr env)
+
+    :else
+    (throw (Exception. (pr-str "Expected primary expression, got: " expr)))))
 
 (defn interpret-definition [definition env]
   (println "Interpreting definition" definition)
-  (interpret-application-expression (:expression definition) env))
+  (let [result (interpret-application-expression (:expression definition) env)]
+    {:name       (:name definition)
+     :definition (pretty-print (:expression definition))
+     :function   (if (and (map? result) (contains? result :function))
+                   (:function result)
+                   result)}))
 
 (defn interpret-definitions [definitions]
   (println "Interpreting definitions" definitions)
@@ -81,19 +109,7 @@
   (let [env (interpret-definitions (:definitions parsed))]
     (interpret-application-expression (:expression parsed) env)))
 
-(defn church->number [cn]
-  (try
-    (let [result ((cn inc) 0)]
-      (println "church->number: result =" result)
-      (if (and (integer? result) (>= result 0))
-        result
-        (throw (Exception. (str "Invalid Church numeral result: " result)))))
-    (catch Exception e
-      (println "Caught exception in church->number:" (.getMessage e))
-      -1)))
-
 ;; --- Main Execution ---
-
 (defn run []
   (if (empty? *command-line-args*)
     (println "Usage: clojure -m interpreter <filename>")
@@ -101,14 +117,14 @@
           input (slurp filename)
           parsed (parser/parse input)]
       (if (:success parsed)
-        (let [prog (:result parsed)
+        (let [prog        (:result parsed)
               eval-result (interpret-program prog)]
-          (println "Parsed definitions:\n" (:definitions prog))
-          (println "Parsed expression:" (:expression prog))
+          (println "Parsed definitions:\n" (pretty-print (:definitions prog)))
+          (println "Parsed expression:" (pretty-print (:expression prog)))
           (try
-            (println "Evaluated result" (church->number eval-result))
+            (println "Evaluated result" (pretty-print eval-result))
             (catch Exception e
-              (println "Evaluated result (as combinatory form):" (pretty-print eval-result)))))
+              (println "Error" e "Evaluated result (as combinatory form):" (pretty-print eval-result)))))
         (println "Parsing failed:" (:error parsed) (:remaining parsed))))))
 
 (run)
